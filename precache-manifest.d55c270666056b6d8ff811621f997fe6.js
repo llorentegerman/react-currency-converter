f7ef2409c086fd34213d7c02eedfbd388596b007
llorentegerman <llorentegerman@gmail.com>
2020-05-29 21:12:46 +1000

    Updates

---
+++ precache-manifest.d55c270666056b6d8ff811621f997fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbe63de79a876af86a6b1f80a5e115f",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "78bef86751b26ac955c6",
+    "url": "/react-currency-converter/static/css/main.df59ed65.chunk.css"
+  },
+  {
+    "revision": "ec5c0466bc6c9e9372b5",
+    "url": "/react-currency-converter/static/js/2.39b2fe73.chunk.js"
+  },
+  {
+    "revision": "387f44d73a90cb3ba8584c397c21a95f",
+    "url": "/react-currency-converter/static/js/2.39b2fe73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bef86751b26ac955c6",
+    "url": "/react-currency-converter/static/js/main.75e6f522.chunk.js"
+  },
+  {
+    "revision": "06ae5a8d29a3761c1243",
+    "url": "/react-currency-converter/static/js/runtime-main.da2e8f1d.js"
+  }
+]);
